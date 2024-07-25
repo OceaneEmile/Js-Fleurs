19f8077230ff3a15bff264d7951ae5e639bb1b31
oceaneEmile <oceane.emile@oclock.school>
2024-07-25 19:07:06 +0200

    Search bar

---
+++ app.js
@@ -0,0 +1,7 @@
+import { Search } from './modules/Search.js'; // j'importe la classe
+
+console.log('App')
+
+
+new Search(); // j'instancie la classe Search
+
---
+++ modules/Search.js
@@ -0,0 +1,12 @@
+
+const moduleSearch = 'module Search'
+
+export { moduleSearch }; // export the moduleSearch variable
+
+class Search {
+
+    constructor() {
+      console.log('Je suis dans le constructor de Search');
+    }
+  }
+  export { Search }; //exporte la classeSs
