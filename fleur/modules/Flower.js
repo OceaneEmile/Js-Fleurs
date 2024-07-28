301d6ca1fed76528a8d66ab5ce6c2e091cbf6712
oceaneEmile <oceane.emile@oclock.school>
2024-07-28 20:10:46 +0200

    cube move with the mouse interraction

--- fleur/modules/Flower.js
+++ fleur/modules/Flower.js
@@ -1,61 +1,64 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 class Flower {
     constructor() {
-        // console.log('Flower constructor');
-        this.canvas=document.querySelector('.js-canvas');
-        this.canvasWidth = window.innerWidth;
-        this.canvasHeight = window.innerHeight;
+        this.canvas = document.querySelector('.js-canvas')
+        this.canvasWidth = window.innerWidth
+        this.canvasHeight = window.innerHeight
 
-        this.init();
+        this.init()
     }
+
     init() {
-        this.createScene();
-        this.camera();
-        this.createRenderer();
-        this.createCube();
-        this.animate();
-    }
+        this.createScene()
+        this.createCamera()
+        this.createRender()
+        this.createOrbitControls()
+        this.createCube()
+        this.animate()
+    } 
+
     createScene() {
-        this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color('skyblue');
-       
+        this.scene = new THREE.Scene()
+        this.scene.background = new THREE.Color('skyblue')
     }
-    camera() {
-        const aspectRatio = this.canvasWidth /this.canvasHeight;
-        this.camera = new THREE.PerspectiveCamera( 75,aspectRatio, 0.1, 1000 );
-        this.camera.position.z = 5;
-        this.camera.position.y = 0;
-        this.camera.position.x = 0;
+
+    createCamera() {
+        const aspectRatio = this.canvasWidth / this.canvasHeight
+        this.camera = new THREE.PerspectiveCamera( 45, aspectRatio, 0.1, 1000 )
+        this.camera.position.z = 50
+        this.camera.position.y = 0
+        this.camera.position.x = 0
+    } 
+
+    createOrbitControls() {
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement )
     }
-    createRenderer() {
+
+    createRender() {
         this.renderer = new THREE.WebGLRenderer()
         this.renderer.setSize( this.canvasWidth, this.canvasHeight )
         this.canvas.appendChild( this.renderer.domElement )
-        
     }
-    createCube() {
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
-    const cubeColor = new THREE.Color('red')
-    const material = new THREE.MeshBasicMaterial( {color: cubeColor} ); 
 
-    this.cube = new THREE.Mesh( geometry, material ); 
-    this.cube.rotation.x = Math.PI * 0.25;
-    this.cube.rotation.y = Math.PI * 0.25;
-    this.scene.add( this.cube );
-        
-    }
+    createCube () {
+        const geometry = new THREE.BoxGeometry( 10, 10, 10 )
+        const color = new THREE.Color('red') 
+        const material = new THREE.MeshBasicMaterial( {color: color} )
+        this.cube = new THREE.Mesh( geometry, material )
+        this.cube.rotation.x = Math.PI * 0.25
+        this.cube.rotation.y = Math.PI * 0.25
+        this.scene.add( this.cube )
+    } 
+
     animate() {
+        requestAnimationFrame( this.animate.bind(this) )
 
-  requestAnimationFrame( this.animate.bind(this) );
-  // console.log('animate')
-  this.renderer.render( this.scene, this.camera );
+        this.controls.update()
 
-  // rotation du cube
-  this.cube.rotation.x += 0.01;
-  this.cube.rotation.y += 0.01;
-  this.renderer.render( this.scene, this.camera );
-       
+        this.renderer.render( this.scene, this.camera )
     }
 }
-export { Flower }; // export the Flower class
+
+export { Flower }
